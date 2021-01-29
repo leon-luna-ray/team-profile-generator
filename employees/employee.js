3c75428f25cccaac13ac15b58cb090f903969e3b
Ray Luna <leon.luna.ray@gmail.com>
2021-01-28 22:04:06 -0800

    Updated files

---
+++ employees/employee.js
@@ -0,0 +1,10 @@
+// add employee consturctor here with the build html funciton.
+// Don't forget module exports.
+
+const Employee = {
+    this.name = name;
+}
+
+function buildHtml(){
+    return`<h1>Add HTML code here with variables ${Employee}`
+};
