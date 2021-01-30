1a8f20bd07dc8fbd23eec02ee2702649998ff987
Ray Luna <leon.luna.ray@gmail.com>
2021-01-29 21:05:29 -0800

    successfully got the app to add three employee types, bug with the intern currently.

--- app.js
+++ app.js
@@ -17,13 +17,17 @@ function newEmployee(){
             type: 'list',
             name: 'employeeType',
             message: 'What type of employee would you like to create?', 
-            choices: ['Engineer', 'Intern', 'Manger', 'Build Team']
+            choices: ['Engineer', 'Intern', 'Manager', 'Build Team']
         }
     ).then(data => {
         if(data.employeeType === 'Engineer') {
             buildEngineer();
-        } // Finish the if/else statement invoking the build employee functions.
-    }) 
+        } else if (data.employeeType === 'Intern') {
+            buildIntern();
+        } else if (data.employeeType === 'Manager') {
+            buildManager();
+        } 
+    }) // .then
 }; // newEmployee
 
 // Build new employee profile based on role.
@@ -56,9 +60,76 @@ function buildEngineer(){
         //Push to the team array.
         team.push(engineer);
         console.log(team);
-    }) // then
+    }) // .then
 }; // buildEngineer
 
+function buildIntern(){
+    inquirer.prompt(
+        [ {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter intern name.'
+        }, 
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter intern id.'
+        },         
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the intern\'s e-mail address?'
+        }, 
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the intern\'s school?'
+        }]
+    ).then(data => {
+        // Create new instance of Intern with the inquirer response.
+        let intern = new Intern(data.name, data.id, data.email, data.school);
 
-//Invoke newEmployee function.
+        //Push to the team array.
+        team.push(Intern);
+        console.log(team);
+    }) // .then
+}; // buildIntern
+
+function buildManager(){
+    inquirer.prompt(
+        [ {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter manager name.'
+        }, 
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter manager id.'
+        },         
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the manager\'s e-mail address?'
+        }, 
+        {
+            type: 'input',
+            name: 'number',
+            message: 'What is the manager\'s office number?'
+        }]
+    ).then(data => {
+        // Create new instance of Manager with the inquirer response.
+        let manager = new Manager(data.name, data.id, data.email, data.number)
+
+        //Push to the team array.
+        team.push(manager);
+        console.log(team);
+    }) // .then
+}; // buildManager
+
+
+
+// This data is passed into an object and pushed to the team array. From there it will be used with the helper functions in the src folder to build the HTML.
+
+//Invoke newmanager function.
 newEmployee();
