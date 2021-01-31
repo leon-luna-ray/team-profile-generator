615f68b3d4d64e23c07742495ca3db76eea8ed22
Ray Luna <leon.luna.ray@gmail.com>
2021-01-31 14:44:19 -0800

    set up first test

---
+++ tests/employee.test.js
@@ -0,0 +1,11 @@
+describe('Description of what is being tested', () => {
+    test('Description of what the test should do', () => {
+        // test code goes here
+    });
+});
+
+// assign value of whatever the expected output will be
+const output = [{}];
+
+// Expect statment will check the value of the input
+expect(className(input, 'input-type')).toEqual(output);
