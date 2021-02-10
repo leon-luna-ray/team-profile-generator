aed6851a9d634163b7ba36772d1e4a4a5fd6b1e7
Ray Luna <leon.luna.ray@gmail.com>
2021-02-10 15:45:13 -0800

    successfully got correclty formatted page with employees

--- src/template.js
+++ src/template.js
@@ -2,124 +2,117 @@
 
 function engineerTemplate(member){
     return `
-    <div class="card">
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder role icon"> <!--find icons to add for each role-->
-          </figure>
+    <div class="column">
+    <div class="card engineer-card">
+        <div class="card-content employee-name">
+            <h2 class="title">${member.name}</h2>
+            <h3 class="subtitle"><i class="fas fa-laptop-code"></i> ${member.role}</h3>
         </div>
-        <div class="media-content">
-          <p class="title is-4">${member.name}</p>
-          <p class="subtitle is-6">${member.role}</p>
+        <div class="card-content contact-info">
+            <ul class="employee-details">
+                <li>ID: ${member.id}</li>
+                <li>E-Mail: ${member.email}</li>
+                <li>Github: ${member.github}</li>
+            </ul>
         </div>
-      </div>
-  
-      <div class="content">
-        <ul style="list-style-type: none;">
-            <li>ID: ${member.id}</li>
-            <li>E-mail: <a href="mailto:${member.email}">${member.email}</a></li>
-            <li>Github: <a href="https://www.github.com/${member.github}>${member.github}</a></li>
-            </ul> 
-      </div>
     </div>
-  </div>
+  </div><!--column/engineer card-->
+
    `
 };
 
 function internTemplate(member){
-    return `
-    <div class="card">
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder role icon"> <!--find icons to add for each role-->
-          </figure>
-        </div>
-        <div class="media-content">
-          <p class="title is-4">${member.name}</p>
-          <p class="subtitle is-6">${member.role}</p>
-        </div>
+  return `
+  <div class="column">
+  <div class="card intern-card">
+      <div class="card-content employee-name">
+          <h2 class="title">${member.name}</h2>
+          <h3 class="subtitle"><i class="fas fa-laptop-code"></i> ${member.role}</h3>
       </div>
-  
-      <div class="content">
-        <ul style="list-style-type: none;">
-            <li>ID: ${member.id}</li>
-            <li>E-mail: <a href="mailto:${member.email}">${member.email}</a></li>
-            <li>School: ${member.school}</a></li>
-            </ul> 
+      <div class="card-content contact-info">
+          <ul class="employee-details">
+              <li>ID: ${member.id}</li>
+              <li>E-Mail: ${member.email}</li>
+              <li>School: ${member.school}</li>
+          </ul>
       </div>
-    </div>
   </div>
-   `
+</div><!--column/engineer card-->
+
+ `
 };
 
 function managerTemplate(member){
     return `
-    <div class="card">
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder role icon"> <!--find icons to add for each role-->
-          </figure>
-        </div>
-        <div class="media-content">
-          <p class="title is-4">${member.name}</p>
-          <p class="subtitle is-6">${member.role}</p>
-        </div>
+    <div class="column">
+      <div class="card manager-card">
+          <div class="card-content employee-name">
+              <h2 class="title">${member.name}</h2>
+              <h3 class="subtitle"><i class="fas fa-briefcase"></i> ${member.role}</h3>
+          </div>
+          <div class="card-content contact-info">
+              <ul class="employee-details">
+                  <li>ID: ${member.id}</li>
+                  <li>E-Mail: ${member.email}</li>
+                  <li>Office Number: ${member.officeNumber}</li>
+              </ul>
+          </div>
       </div>
-  
-      <div class="content">
-        <ul style="list-style-type: none;">
-            <li>ID: ${member.id}</li>
-            <li>E-mail: <a href="mailto:${member.email}">${member.email}</a></li>
-            <li>Office Number: ${member.number}</a></li>
-            </ul> 
-      </div>
-    </div>
-  </div>
+    </div><!--column/manager card-->
+
    `
 };
 
-
 function generateMarkup(team) {
+    let employeeCards = [];
     team.forEach(member => {
         if (member.role === 'Engineer') {
             let engineerCard = engineerTemplate(member);
-            handleMarkup(engineerCard);
+            employeeCards.push(engineerCard);
         } else if (member.role === 'Intern') {
             let internCard = internTemplate(member);
-            console.log(internCard);
+            employeeCards.push(internCard);
         } else if (member.role === 'Manager') {
             let managerCard = managerTemplate(member);
-            console.log(managerCard);
+            employeeCards.push(managerCard);
         };
     });
+    // Take the array and remove commas with join.
+    employeeCards = employeeCards.join('');
+    const data = handleMarkup(employeeCards);
+    return data;
+ };
 
-    function handleMarkup(card) {
-        // Need to find a way to append each card to this template then create and push to the HTML file.
-        let markup = `
+    function handleMarkup(cards) {
+        return `
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
-            <title>Team Page</title>
+            <title>My Team</title>
         </head>
         <body>
-            <div class="employee-grid">
-                ${card}
-            </div>
+            <header class="hero is-primary">
+                <div class="hero-body">
+                    <div class="container">
+                        <h1 class="title is-1 page-title center">My Team</h1>
+                    </div>
+                </div>
+            </header> <!--header-->
+
+            <main class="container">
+            <div class="columns">
+              ${cards}
+            </div><!--columns/employee cards-->
+        </main> <!--container-->
         </body>
         </html>
         `
-        console.log(markup)
-    }; //handleMakrup`
-};
+    }; //handleMakrup
+
 
 module.exports = generateMarkup;
